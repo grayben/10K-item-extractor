ffc68c2b25eab372736a3f838f1715b314f65cdf
Ben Gray <gray.ben.m@gmail.com>
2015-11-17 18:37:49 +1100

    Add scorers

---
+++ src/com/grayben/riskExtractor/HtmlScorer/SegmentationElementScorer.java
@@ -0,0 +1,13 @@
+package com.grayben.riskExtractor.HtmlScorer;
+
+import org.jsoup.nodes.Element;
+
+public class SegmentationElementScorer implements Scorer<Element> {
+
+	@Override
+	public int score(Element input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
---
+++ src/com/grayben/riskExtractor/HtmlScorer/TagAndAttributeScorer.java
@@ -0,0 +1,11 @@
+package com.grayben.riskExtractor.HtmlScorer;
+
+public class TagAndAttributeScorer implements Scorer<TagAndAttribute> {
+
+	@Override
+	public int score(TagAndAttribute input) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
