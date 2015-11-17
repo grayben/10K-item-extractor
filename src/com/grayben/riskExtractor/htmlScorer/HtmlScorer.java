9834c30d624cb90c0fe5c43c4dc62099594e827a
Ben Gray <gray.ben.m@gmail.com>
2015-11-17 19:26:47 +1100

    Remove crap from htmlScorer

--- src/com/grayben/riskExtractor/htmlScorer/HtmlScorer.java
+++ src/com/grayben/riskExtractor/htmlScorer/HtmlScorer.java
@@ -15,38 +15,8 @@ public class HtmlScorer {
 	NodeVisitor nv;
 
 	public static void main(String[] args) throws IOException {
-		System.out.println("Starting parse");
-		File input = null;
-		Document doc = null;
-		int numDocuments = 100;
-		
-		// 
-					doc = Jsoup.connect("http://google.com/").get();
-					Elements elements = doc.select("*");
-					String toHtml = elements.text();
-					System.out.print(toHtml);
-					
-					NodeVisitor flattener = new ScoringAndFlatteningNodeVisitor();
-					NodeTraversor nt = new NodeTraversor(flattener);
-					nt.traverse(doc);
-		
-		final long startTime = System.currentTimeMillis();
-		for(int i = 1; i <= numDocuments; i++){
-			input = new File("/Volumes/MBS Data/EDGAR-Form10K/filings/" + i + ".txt");
-			doc = Jsoup.parse(input, "ASCII");
-			elements = doc.select("*");
-			String html = elements.text();
-			System.out.print(html);
-			
-			
-		}
-		final long endTime = System.currentTimeMillis();
-		final long dT = endTime - startTime;
-		final long secondsElapsed = dT/1000;
-		System.out.println("Finished parsing: " 
-				+ numDocuments + " in " + secondsElapsed + " seconds.");
-		
-		
+		Document doc = Jsoup.connect("http://google.com/").get();
 	}
-
+	
+	
 }
