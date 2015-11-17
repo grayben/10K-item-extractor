5fd4553335e632edc8155e12b28ddd513dca6fe1
Ben Gray <gray.ben.m@gmail.com>
2015-11-17 19:04:55 +1100

    Rename ElementScorer

---
+++ src/com/grayben/riskExtractor/htmlScorer/elementScorers/ElementScorer.java
@@ -0,0 +1,19 @@
+package com.grayben.riskExtractor.htmlScorer.elementScorers;
+
+
+/**
+ * Element scorer interface. Provide an implementing class to {@link ScoringAndFlatteningVisitor}.
+ * <p/>
+ * This interface contains a single method that takes an {@link org.jsoup.nodes.Element} and returns a score.
+ * @author beng
+ *
+ */
+public interface ElementScorer<T> {
+	
+	/**
+	 * 
+	 * @param element the Element to score
+	 * @return the score of element
+	 */
+	public int score(T input);
+}
