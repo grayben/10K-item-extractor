767028a3ac7db70c055f1f981a39881a464da02c
Ben Gray <gray.ben.m@gmail.com>
2015-11-18 17:45:17 +1100

    Add classes implementing data transfer interfaces

---
+++ src/com/grayben/riskExtractor/headingIdentifier/NominatedTextList.java
@@ -0,0 +1,13 @@
+package com.grayben.riskExtractor.headingIdentifier;
+
+import java.util.List;
+
+public class NominatedTextList implements NomineeRetrievable {
+
+	@Override
+	public List<Integer> getNomineeIndexes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/com/grayben/riskExtractor/headingSelector/ElectedTextList.java
@@ -0,0 +1,19 @@
+package com.grayben.riskExtractor.headingSelector;
+
+import java.util.List;
+
+public class ElectedTextList implements NomineeAndElecteeRetrievable {
+
+	@Override
+	public List<Integer> getNomineeIndexes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Integer> getElecteeIndexes() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
