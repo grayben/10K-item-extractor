a79d9f347d46a5dffdeb8a46cf6d169285dbbbce
Ben Gray <gray.ben.m@gmail.com>
2015-11-18 17:42:39 +1100

    Add combination interface

---
+++ src/com/grayben/riskExtractor/headingSelector/NomineeAndElecteeRetrievable.java
@@ -0,0 +1,7 @@
+package com.grayben.riskExtractor.headingSelector;
+
+import com.grayben.riskExtractor.headingIdentifier.NomineeRetrievable;
+
+public interface NomineeAndElecteeRetrievable extends NomineeRetrievable, ElecteeRetrievable {
+
+}
