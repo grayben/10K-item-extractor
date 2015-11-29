f54e260d1e4e282fb34c3e245dba4e57583b3256
Ben Gray <gray.ben.m@gmail.com>
2015-11-29 15:51:56 +1100

    Make class pass tests

--- src/main/java/com/grayben/riskExtractor/headingMarker/nominator/NominatedTextList.java
+++ src/main/java/com/grayben/riskExtractor/headingMarker/nominator/NominatedTextList.java
@@ -5,13 +5,30 @@ import java.util.List;
 import com.grayben.riskExtractor.headingMarker.TextCandidate;
 
 public class NominatedTextList implements NomineesRetrievable {
-	
+
+	List<String> textList;
+
+	public List<String> getTextList() {
+		return textList;
+	}
+
+	public void setTextList(List<String> textList) {
+		this.textList = textList;
+	}
+
 	List<TextCandidate> nominees;
+
+	public void setNominees(List<TextCandidate> nominees) {
+		this.nominees = nominees;
+	}
 
 	@Override
 	public List<TextCandidate> getNominees() {
-		// TODO Auto-generated method stub
-		return null;
+		return this.nominees;
 	}
 
+    public NominatedTextList(List<String> textList, List<TextCandidate> nominees) {
+        this.textList = textList;
+        this.nominees = nominees;
+    }
 }
