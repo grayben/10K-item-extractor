fa7a7f2fe5099655a14fd7a5afe38473507951d9
Ben Gray <gray.ben.m@gmail.com>
2015-11-29 14:40:45 +1100

    Add some tests to ElectedTextList class

--- src/test/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextListTest.java
+++ src/test/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextListTest.java
@@ -1,21 +1,36 @@
 package com.grayben.riskExtractor.headingMarker.elector;
 
+import com.grayben.riskExtractor.headingMarker.TextCandidate;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by beng on 28/11/2015.
  */
-@Ignore
 public class ElectedTextListTest {
+
+    ElectedTextList electedTextList;
+
+    @Mock
+    protected List<String> stringListMock;
+
+    @Mock
+    protected List<TextCandidate> nomineesMock;
+
+    @Mock
+    protected List<TextCandidate> electeesMock;
 
     @Before
     public void setUp() throws Exception {
-
+        electedTextList = new ElectedTextList(stringListMock, nomineesMock, electeesMock);
     }
 
     @After
@@ -24,12 +39,43 @@ public class ElectedTextListTest {
     }
 
     @Test
-    public void testGetNominees() throws Exception {
+    public void test_GetNominees_ReturnsNonNull_Always() throws Exception {
+        //setup data
 
+        //setup expectations
+
+        //exercise
+        List<TextCandidate> nominees = electedTextList.getNominees();
+        //verify
+        assertNotNull(nominees);
     }
 
     @Test
-    public void testGetElectees() throws Exception {
+    public void test_GetNominees_ReturnsConstructedNominees_OnlyWhenNoOverwrite() throws Exception {
+        //setup data
+        List<TextCandidate> newNomineesMock = (List<TextCandidate>) Mockito.mock(List.class);
 
+        //setup expectations
+
+        //exercise
+        List<TextCandidate> nomineesReturned = electedTextList.getNominees();
+        electedTextList.setNominees(newNomineesMock);
+        List<TextCandidate> newNomineesReturned = electedTextList.getNominees();
+
+        //verify
+        assertEquals(nomineesMock, nomineesReturned);
+        assertEquals(newNomineesMock, newNomineesReturned);
+        assertNotEquals(nomineesMock, newNomineesReturned);
+    }
+
+    @Test
+    public void test_GetElectees_ReturnsNonNull() throws Exception {
+        //setup data
+
+        //setup expectations
+
+        //exercise
+
+        //verify
     }
 }
