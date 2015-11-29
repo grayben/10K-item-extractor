a468298d5f023d36f5b96bf62976fbf596a468a6
Ben Gray <gray.ben.m@gmail.com>
2015-11-29 16:15:51 +1100

    Write test

--- src/test/java/com/grayben/riskExtractor/headingMarker/MarkedTextTest.java
+++ src/test/java/com/grayben/riskExtractor/headingMarker/MarkedTextTest.java
@@ -1,22 +1,30 @@
 package com.grayben.riskExtractor.headingMarker;
 
+import com.grayben.riskExtractor.headingMarker.elector.ElectedTextList;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by beng on 28/11/2015.
  */
+@RunWith(MockitoJUnitRunner.class)
 public abstract class MarkedTextTest {
 
+    @Mock
+    ElectedTextList electedTextListMock;
 
-
+    MarkedText markedText;
     @Before
     public void setUp() throws Exception {
-
+        markedText = new MarkedText(electedTextListMock);
     }
 
     @After
@@ -25,7 +33,8 @@ public abstract class MarkedTextTest {
     }
 
     @Test
-    public void testSubselection() throws Exception {
-
+    public void test_Subselections_ReturnNonNull_Always() throws Exception {
+        List<String> subselections = markedText.subselections();
+        assertNotNull(subselections);
     }
 }
