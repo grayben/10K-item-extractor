2e0b44461408ad67f16791c8c89e3d2cc3e61901
Ben Gray <gray.ben.m@gmail.com>
2015-11-29 14:38:05 +1100

    Add mutators

--- src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextList.java
+++ src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextList.java
@@ -10,27 +10,45 @@ public class ElectedTextList implements
 	ElecteesRetrievable {
 
 	List<String> textList;
-	
+
+    public List<String> getTextList() {
+        return textList;
+    }
+
+    public void setTextList(List<String> textList) {
+        this.textList = textList;
+    }
+    
 	List<TextCandidate> nominees;
-	List<TextCandidate> electees;
+
+    @Override
+    public List<TextCandidate> getNominees() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setNominees(List<TextCandidate> nominees) {
+        this.nominees = nominees;
+    }
+
+
+    List<TextCandidate> electees;
+
+    @Override
+    public List<TextCandidate> getElectees() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public void setElectees(List<TextCandidate> electees) {
+        this.electees = electees;
+    }
 
 	public ElectedTextList(List<String> textList, List<TextCandidate> nominees, List<TextCandidate> electees){
 		super();
 		this.textList = textList;
 		this.nominees = nominees;
 		this.electees = electees;
-	}
-
-	@Override
-	public List<TextCandidate> getNominees() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public List<TextCandidate> getElectees() {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
