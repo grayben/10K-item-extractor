f38e13acd2819c6fd34801fd6a3c5ad1ff4196ce
Ben Gray <gray.ben.m@gmail.com>
2015-11-29 14:13:18 +1100

    Make ElectedTextList concrete

--- src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextList.java
+++ src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElectedTextList.java
@@ -5,12 +5,21 @@ import java.util.List;
 import com.grayben.riskExtractor.headingMarker.TextCandidate;
 import com.grayben.riskExtractor.headingMarker.nominator.NomineesRetrievable;
 
-public abstract class ElectedTextList implements
+public class ElectedTextList implements
 	NomineesRetrievable,
 	ElecteesRetrievable {
+
+	List<String> textList;
 	
 	List<TextCandidate> nominees;
 	List<TextCandidate> electees;
+
+	public ElectedTextList(List<String> textList, List<TextCandidate> nominees, List<TextCandidate> electees){
+		super();
+		this.textList = textList;
+		this.nominees = nominees;
+		this.electees = electees;
+	}
 
 	@Override
 	public List<TextCandidate> getNominees() {
