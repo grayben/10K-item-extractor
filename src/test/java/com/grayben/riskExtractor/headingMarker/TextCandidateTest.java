22a6af3e1d5ecf3b9ba0c3be0f6c08bb3a423431
Ben Gray <gray.ben.m@gmail.com>
2015-12-01 16:29:02 +1100

    Implement tests for immutability

--- src/test/java/com/grayben/riskExtractor/headingMarker/TextCandidateTest.java
+++ src/test/java/com/grayben/riskExtractor/headingMarker/TextCandidateTest.java
@@ -7,7 +7,11 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Comparator;
 import java.util.List;
+import java.util.function.UnaryOperator;
 
 import static org.junit.Assert.*;
 
@@ -26,9 +30,14 @@ public class TextCandidateTest {
 
     int index = 0;
 
+    int listSize = 50;
+
     @Mock
     List<String> list;
-    int listSize = 50;
+
+    @Mock
+    Collection<String> collectionOfStrings;
+
 
     @Before
     public void setUp() throws Exception {
@@ -78,69 +87,89 @@ public class TextCandidateTest {
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenAddObject
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.add(new String());
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenAddObjectWithIndex
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.add(0, new String());
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenAddAllCollection
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.addAll(collectionOfStrings);
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenAddAllCollectionWithIndex
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.addAll(0, collectionOfStrings);
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenClear
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.clear();
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenRemoveAll
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.removeAll(collectionOfStrings);
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenReplaceAll
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.replaceAll(new UnaryOperator<String>() {
+            @Override
+            public String apply(String s) {
+                return null;
+            }
+        });
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenRetainAll
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.retainAll(collectionOfStrings);
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenSet
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.set(0, "cow");
     }
 
     @Test
     public void
     test_ReturnedTextListThrowsUnsupportedOperationException_WhenSort
             () throws Exception {
-        fail("This test has not been implemented");
+        thrown.expect(UnsupportedOperationException.class);
+        stringListReturned.sort(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return o1.hashCode() - o2.hashCode();
+            }
+        });
     }
 }
