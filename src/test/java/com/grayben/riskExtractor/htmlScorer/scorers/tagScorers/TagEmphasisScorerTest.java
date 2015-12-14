cbc9bd528b13a9cddcf786951260001d53418b7e
Ben Gray <gray.ben.m@gmail.com>
2015-12-14 14:47:59 +1100

    Align with new base class implementation

--- src/test/java/com/grayben/riskExtractor/htmlScorer/scorers/tagScorers/TagEmphasisScorerTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/scorers/tagScorers/TagEmphasisScorerTest.java
@@ -1,25 +1,35 @@
 package com.grayben.riskExtractor.htmlScorer.scorers.tagScorers;
 
+import com.grayben.riskExtractor.htmlScorer.scorers.ScorerTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 /**
  * Created by beng on 28/11/2015.
  */
 @Ignore
-public class TagEmphasisScorerTest {
+public class TagEmphasisScorerTest
+        extends ScorerTest {
+
+    public TagEmphasisScorer tagEmphasisScorerSUT;
 
     @Before
     public void setUp() throws Exception {
-
+        this.tagEmphasisScorerSUT = new TagEmphasisScorer
+                (TagEmphasisScorer.defaultMap());
+        super.setUp(tagEmphasisScorerSUT);
     }
 
     @After
     public void tearDown() throws Exception {
+
+    }
+
+    @Override
+    @Test
+    public void test_ScoreReturnsInteger_WhenArgumentIsNonNull() throws Exception {
 
     }
 
