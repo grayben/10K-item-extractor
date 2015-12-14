bea9239959dd0b51332286ae730585902007e9fc
Ben Gray <gray.ben.m@gmail.com>
2015-12-14 14:45:01 +1100

    Add default map

--- src/main/java/com/grayben/riskExtractor/htmlScorer/scorers/tagScorers/TagEmphasisScorer.java
+++ src/main/java/com/grayben/riskExtractor/htmlScorer/scorers/tagScorers/TagEmphasisScorer.java
@@ -5,15 +5,20 @@ import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class TagEmphasisScorer
 		extends Scorer<Tag> {
-	
-	List<Tag> tagScores = null;
 
-	TagEmphasisScorer(String scoreLabel) {
-		super(scoreLabel);
+    public static final String SCORE_LABEL = "tag-emphasis";
+	
+	Map<Tag, Integer> tagScores = null;
+
+	TagEmphasisScorer(Map<Tag, Integer> tagScores) {
+		super(SCORE_LABEL);
+        this.tagScores = tagScores;
 	}
 
 	public int score(Element element) {
@@ -21,18 +26,18 @@ public class TagEmphasisScorer
 		return 0;
 	}
 
-	public void useDefaultMap() {
+	public static final Map<Tag, Integer> defaultMap() {
 		
 		//Map<Tag, Integer> tagScores = this.getTagScores();
-		List<Tag> tags = new ArrayList<Tag>();
-		tags.add(Tag.valueOf("b"));
-		tags.add(Tag.valueOf("strong"));
-		tags.add(Tag.valueOf("h1"));
-		tags.add(Tag.valueOf("h2"));
-		tags.add(Tag.valueOf("h3"));
-		tags.add(Tag.valueOf("h4"));
-		tags.add(Tag.valueOf("h5"));
-		tags.add(Tag.valueOf("u"));
+		Map<Tag, Integer> tags = new HashMap<>();
+		tags.put(Tag.valueOf("b"), 1);
+		tags.put(Tag.valueOf("strong"), 1);
+		tags.put(Tag.valueOf("h1"), 1);
+		tags.put(Tag.valueOf("h2"), 1);
+		tags.put(Tag.valueOf("h3"), 1);
+		tags.put(Tag.valueOf("h4"), 1);
+		tags.put(Tag.valueOf("h5"), 1);
+		tags.put(Tag.valueOf("u"), 1);
 		
 		//this.setTagScores(tagScores);
 		
