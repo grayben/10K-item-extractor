2f7f4acfb4dd8195c3bee075c00443d285c38991
Ben Gray <gray.ben.m@gmail.com>
2015-12-16 15:52:54 +1100

    Add tests

--- src/test/java/com/grayben/riskExtractor/htmlScorer/ScoredTextTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/ScoredTextTest.java
@@ -4,30 +4,87 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.jsoup.helper.Validate.fail;
 
 /**
  * Created by beng on 28/11/2015.
  */
 @Ignore
+@RunWith(MockitoJUnitRunner.class)
 public class ScoredTextTest {
+
+    private ScoredText scoredTextSUT;
+
+    @Mock
+    public ScoredTextElement scoredTextElementMock;
 
     @Before
     public void setUp() throws Exception {
-
+        scoredTextSUT = new ScoredText();
     }
 
     @After
     public void tearDown() throws Exception {
-
+        scoredTextSUT = null;
     }
 
     @Test
-    public void testToString() throws Exception {
-
+    public void
+    test_ToStringReturnsNonNull_WhenNothingWasAdded
+            () throws Exception {
+        fail("Test not implemented");
     }
 
     @Test
-    public void testAdd() throws Exception {
+    public void
+    test_AddChangesWhatToStringReturns
+            () throws Exception {
+        fail("Test not implemented");
+    }
 
+    @Test
+    public void
+    test_AddThrowsNullPointerException_WhenParamIsNull
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_AddThrowsIllegalArgumentException_WhenParamDoesNotHaveScores
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_AddThrowsIllegalArgumentException_WhenParamDoesNotHaveText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_AddThrowsIllegalArgumentException_WhenParamHasEmptyText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_AddThrowsIllegalArgumentException_WhenParamHasEmptyScores
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetTextElementReturnsExpectedOutput_OnSimpleInputCase
+            () throws Exception {
+        fail("Test not implemented");
     }
 }
