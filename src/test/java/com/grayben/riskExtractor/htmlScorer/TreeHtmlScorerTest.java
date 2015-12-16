6ee3e2a92990fc971c7cc71ab0ceda68309740a3
Ben Gray <gray.ben.m@gmail.com>
2015-12-16 14:26:17 +1100

    Scaffold tests

--- src/test/java/com/grayben/riskExtractor/htmlScorer/TreeHtmlScorerTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/TreeHtmlScorerTest.java
@@ -7,25 +7,36 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
+import static junit.framework.TestCase.fail;
+
 /**
  * Created by beng on 28/11/2015.
  */
 @RunWith(MockitoJUnitRunner.class)
 @Ignore
-public class TreeHtmlScorerTest {
+public class TreeHtmlScorerTest
+        extends HtmlScorerTest {
 
     @Before
     public void setUp() throws Exception {
-
+        super.setUp();
+        setHtmlScorer(new TreeHtmlScorer());
     }
 
     @After
     public void tearDown() throws Exception {
-
+        setHtmlScorer(null);
     }
 
+    @Override
     @Test
-    public void testScoreHtml() throws Exception {
+    public void test_ScoreHtmlReturnsExpected_WhenTextInputIsSimple() throws Exception {
+        fail("Test not implemented");
+    }
 
+    @Override
+    @Test
+    public void test_ScoreHtmlReturnsExpected_WhenTextInputIsComplicated() throws Exception {
+        fail("Test not implemented");
     }
 }
