026f668f65e6619d7d8d827c7c62b4be3edcf99e
Ben Gray <gray.ben.m@gmail.com>
2015-12-16 16:47:17 +1100

    Add tests

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/ScoringAndFlatteningNodeVisitorTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/ScoringAndFlatteningNodeVisitorTest.java
@@ -1,11 +1,17 @@
 package com.grayben.riskExtractor.htmlScorer.nodeVisitor;
 
+import com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor;
+import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
+import org.jsoup.nodes.Element;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.jsoup.helper.Validate.fail;
 
 /**
  * Created by beng on 28/11/2015.
@@ -15,9 +21,20 @@ import org.mockito.runners.MockitoJUnitRunner;
 public class ScoringAndFlatteningNodeVisitorTest
         extends NodeVisitorTest {
 
-    @Before
-    public void setUp() throws Exception {
+    private ScoringAndFlatteningNodeVisitor scoringAndFlatteningNodeVisitorSUT;
 
+    @Mock
+    public Scorer<Element>[] elementScorersMock;
+
+    @Before
+    @Override
+    public void setUp() throws Exception {
+        scoringAndFlatteningNodeVisitorSUT
+                = new ScoringAndFlatteningNodeVisitor(
+                elementScorersMock
+        );
+        super.setNodeVisitorSUT(scoringAndFlatteningNodeVisitorSUT);
+        super.setUp();
     }
 
     @After
@@ -26,12 +43,79 @@ public class ScoringAndFlatteningNodeVisitorTest
     }
 
     @Test
-    public void testHead() throws Exception {
-
+    public void
+    test_InitThrowsNullPointerException_WhenElementScorersIsNull
+            () throws Exception {
+        fail("Test not implemented");
     }
 
     @Test
-    public void testTail() throws Exception {
+    public void
+    test_InitThrowsNullPointerException_WhenAnyElementScorerIsNull
+            () throws Exception {
+        fail("Test not implemented");
+    }
 
+    @Test
+    public void
+    test_GetScoredTextReturnsNonNull_ImmediatelyAfterSUTIsInitialised
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsEmpty_ImmediatelyAfterSUTIsInitialised
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsEmpty_AfterSingleVisitToNonElementNode
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsEmpty_AfterSingleVisitToElementWithNoText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsNotEmpty_AfterSingleVisitToElementNodeWithText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsExpectedText_AfterSingleVisitToElementWithText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsExpectedScores_AfterSingleVisitToElementWithText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsExpectedText_AfterVisitsToManyElementsWithText
+            () throws Exception {
+        fail("Test not implemented");
+    }
+
+    @Test
+    public void
+    test_GetScoredTextReturnsExpectedScores_AfterVisitsToManyElementsWithText
+            () throws Exception {
+        fail("Test not implemented");
     }
 }
