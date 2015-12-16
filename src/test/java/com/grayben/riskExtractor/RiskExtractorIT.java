49deff7cab1b8fc838ee7554cfb3a83b60f332d7
Ben Gray <gray.ben.m@gmail.com>
2015-12-16 17:01:53 +1100

    Finished scaffolding all tests

--- src/test/java/com/grayben/riskExtractor/RiskExtractorIT.java
+++ src/test/java/com/grayben/riskExtractor/RiskExtractorIT.java
@@ -4,13 +4,16 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
+import static org.jsoup.helper.Validate.fail;
 
 /**
  * Created by beng on 28/11/2015.
  */
 @Ignore
+@RunWith(MockitoJUnitRunner.class)
 public class RiskExtractorIT {
 
     @Before
@@ -24,7 +27,38 @@ public class RiskExtractorIT {
     }
 
     @Test
-    public void testMain() throws Exception {
+    public void test_MainThrowsIllegalArgumentException_WhenOneArgument
+            () throws Exception {
+        fail("Test not written");
+    }
+
+    @Test
+    public void test_MainThrowsIllegalArgumentException_WhenThreeArguments
+            () throws Exception {
+        fail("Test not written");
+    }
+
+    @Test
+    public void test_MainThrowsFileNotFoundException_WhenInvalidFileArgument
+            () throws Exception {
+        fail("Test not written");
+    }
+
+    @Test
+    public void test_MainCreatesExpectedOutputFile
+            () throws Exception {
+        fail("Test not written");
+    }
+
+    @Test
+    public void test_MainCreatesExpectedOutputFileContents_WhenEasyExample
+            () throws Exception {
+        fail("Test not written");
+    }
+
+    @Test
+    public void test_MainCreatesExpectedOutputFileContents_WhenComplicatedExample
+            () throws Exception {
         fail("Test not written");
     }
 }
