30c5447d19dd64069632f86169f159385150d789
Ben Gray <gray.ben.m@gmail.com>
2015-12-17 15:24:39 +1100

    Skip MapScorer in inheritance heirarchy, since it was not correct semantics

--- src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/elementScorers/SegmentationElementScorer.java
+++ src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/elementScorers/SegmentationElementScorer.java
@@ -1,6 +1,5 @@
 package com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers;
 
-import com.grayben.riskExtractor.htmlScorer.partScorers.MapScorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
@@ -8,7 +7,7 @@ import org.jsoup.parser.Tag;
 /**
  * Score the segmentation of an {@link org.jsoup.nodes.Element} based upon the {@link org.jsoup.parser.Tag} it contains.
  */
-public class SegmentationElementScorer extends MapScorer<Element> {
+public class SegmentationElementScorer extends Scorer<Element> {
 
 	public final static String SCORE_LABEL = "element-segmentation";
 
