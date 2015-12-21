15c8e7f7b7f0473d768ceae951b51e9a3b176bc7
Ben Gray <gray.ben.m@gmail.com>
2015-12-21 13:32:24 +1100

    Move helper methods into a TestHelper class

--- src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/elementScorers/EmphasisElementScorerTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/elementScorers/EmphasisElementScorerTest.java
@@ -1,10 +1,8 @@
 package com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers;
 
 import com.grayben.riskExtractor.htmlScorer.partScorers.ScorerTest;
-import com.grayben.riskExtractor.htmlScorer.partScorers.TagAndAttribute;
 import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagAndAttributeScorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagEmphasisScorer;
-import org.jsoup.nodes.Attribute;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
@@ -16,8 +14,10 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
+import static com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.TestHelper.*;
 import static junit.framework.Assert.assertNotNull;
 
 /**
@@ -45,62 +45,7 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
         super.tearDown();
     }
 
-    private void stubElementToBeScoredMock(Tag tag, Attributes attributes){
-        Mockito.when(elementToBeScoredMock.tag())
-                .thenReturn(tag);
-        Mockito.when(elementToBeScoredMock.attributes())
-                .thenReturn(attributes);
-    }
 
-    private Tag stubTag(String tagName){
-        Tag tag = Mockito.mock(Tag.class);
-        Mockito.when(tag.isEmpty()).thenReturn(false);
-        Mockito.when(tag.getName()).thenReturn("font");
-
-        return tag;
-    }
-
-    private Attribute stubAttribute(String key, String value){
-        Attribute attribute = Mockito.mock(Attribute.class);
-        Mockito.when(attribute.getKey()).thenReturn(key);
-        Mockito.when(attribute.getValue()).thenReturn(value);
-
-        return attribute;
-    }
-
-    private List<Attribute> stubAttributes(Map<String, String> mapping){
-        List<Attribute> attributes = new ArrayList<>();
-        for (Map.Entry<String, String> entry: mapping.entrySet()) {
-            attributes.add(stubAttribute(entry.getKey(), entry.getValue()));
-        }
-
-        assert attributes.size() == mapping.size();
-        return attributes;
-    }
-
-    private Attributes convertListToAttributes(List<Attribute> attributeList){
-        Attributes attributes = new Attributes();
-        for (Attribute attribute :
-                attributeList) {
-            attributes.put(attribute);
-        }
-        return attributes;
-    }
-
-    private Map<String, String> randomMap(int number){
-        Map<String, String> map = new HashMap<>();
-        Random rand = new Random();
-        Iterator<Integer> it = rand.ints(number * 2).iterator();
-        while(it.hasNext()){
-            map.put(
-                    it.next().toString(),
-                    it.next().toString()
-            );
-        }
-        assert map.size() == number;
-
-        return map;
-    }
 
     @Override
     @Test
@@ -114,7 +59,7 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
                 )
         );
 
-        stubElementToBeScoredMock(tag, attributes);
+        elementToBeScoredMock = stubElement(tag, attributes);
 
         Integer returned = emphasisElementScorerSUT.score(elementToBeScoredMock);
 
@@ -133,7 +78,9 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
                 )
         );
 
-        stubElementToBeScoredMock(null, attributes);
+        elementToBeScoredMock = stubElement(null, attributes);
+
+        //TODO: make this test run
     }
 
     @Test
@@ -141,7 +88,9 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
     test_ScoreThrowsNullPointerException_WhenAttributesIsNull() throws Exception {
         thrown.expect(NullPointerException.class);
 
-        stubElementToBeScoredMock(stubTag("font"), null);
+        elementToBeScoredMock = stubElement(stubTag("font"), null);
+
+        //TODO: make this test run
     }
 
     @Test
@@ -157,7 +106,7 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
                 )
         );
 
-        stubElementToBeScoredMock(tagStub, attributes);
+        elementToBeScoredMock = stubElement(tagStub, attributes);
 
         thrown.expect(IllegalArgumentException.class);
 
@@ -172,123 +121,31 @@ public class EmphasisElementScorerTest extends ScorerTest<Element> {
         // GENERATE OUTPUT ////////////////////////////////////////
         Map<Element, Integer> expectedOutput = new HashMap<>();
 
-        Map<TagAndAttribute, Integer> tagAndAttributeScoreMap
-                = TagAndAttributeScorer.defaultMap();
-
         //add Map.Entry<Element, Integer> entries to expectedOutput
         //based on the tagAndAttributeScoreMap
         Map<Element, Integer> scoredMapBasedOnTagAndAttributes
-                = mockElementsAndScoredConformingToTagAndAttributeScoreMap(
-                tagAndAttributeScoreMap
+                = mockElementsAndScoresConformingToTagAndAttributeScoreMap(
+                TagAndAttributeScorer.defaultMap()
         );
-
-
-        Map<Tag, Integer> tagEmphasisScoreMap
-                = TagEmphasisScorer.defaultMap();
+        expectedOutput.putAll(scoredMapBasedOnTagAndAttributes);
 
         //add Map.Entry<Element, Integer> entries to expectedOutput
         //based on the tagEmphasisScoreMap
         Map<Element, Integer> scoresMapBasedOnTags
                 = mockElementsAndScoresConformingToTagScoreMap(
-                tagEmphasisScoreMap
+                TagEmphasisScorer.defaultMap()
         );
-
-        //combine the two maps
         expectedOutput.putAll(scoresMapBasedOnTags);
-        expectedOutput.putAll(scoredMapBasedOnTagAndAttributes);
-        assert expectedOutput.size() ==
-                tagAndAttributeScoreMap.size() + tagEmphasisScoreMap.size();
+
+        assert expectedOutput.size()
+                == scoredMapBasedOnTagAndAttributes.size()
+                + scoresMapBasedOnTags.size();
         ///////////////////////////////////////////////////////////
 
         //TODO: define input
     }
 
-    private Element
-    mockElementConformingToTagAndAttribute(TagAndAttribute tagAndAttribute){
-        Element elementMock = Mockito.mock(Element.class);
 
-        //the element returns the scored TagAndAttribute.getTag()
-        Mockito.when(elementMock.tag()).thenReturn(tagAndAttribute.getTag());
-
-        //generate the target attribute from the scored key
-        Attribute targetAttributeMock = stubAttribute(
-                tagAndAttribute.getAttribute().getKey(),
-                tagAndAttribute.getAttribute().getValue()
-        );
-
-        //Stub the Attributes randomly - these ones don't matter
-        Attributes attributeMocks = convertListToAttributes(
-                stubAttributes(randomMap(5))
-        );
-
-        //but then add the target attribute
-        attributeMocks.put(targetAttributeMock);
-
-        //the element returns the attributes
-        // containing TagAndAttribute.getAttribute();
-        Mockito.when(elementMock.attributes()).thenReturn(attributeMocks);
-
-        return elementMock;
-    }
-
-    private Map<Element, Integer>
-            mockElementsAndScoredConformingToTagAndAttributeScoreMap
-            (Map<TagAndAttribute, Integer> scoreMap){
-        Map<Element, Integer> elementsAndScores = new HashMap<>();
-
-        for (Map.Entry<TagAndAttribute, Integer> entry : scoreMap.entrySet()){
-            Element elementMock = mockElementConformingToTagAndAttribute(
-                    entry.getKey()
-            );
-
-            //expect the element mock to be scored
-            //according to the tagAndAttribute in the map
-            elementsAndScores.put(elementMock, entry.getValue());
-        }
-
-        assert elementsAndScores.size() == scoreMap.size();
-
-        return elementsAndScores;
-    }
-
-    private Element mockElementConformingToTag(Tag tag){
-        Element elementMock = Mockito.mock(Element.class);
-
-        //the element returns the scored Tag
-        Mockito.when(elementMock.tag()).thenReturn(tag);
-
-        //add some dummy attributes to the element
-        //(they shouldn't matter, since only the tag is important)
-        Attributes attributes = convertListToAttributes(
-                stubAttributes(
-                        randomMap(2)
-                )
-        );
-        Mockito.when(elementMock.attributes()).thenReturn(attributes);
-
-        return elementMock;
-    }
-
-    private Map<Element, Integer>
-            mockElementsAndScoresConformingToTagScoreMap
-            (Map<Tag, Integer> scoreMap){
-
-        Map<Element, Integer> elementsAndScores = new HashMap<>();
-
-        for (Map.Entry<Tag, Integer> entry:
-                scoreMap.entrySet()) {
-
-            Element elementMock = mockElementConformingToTag(entry.getKey());
-
-            // expect the element mock to be scored
-            // according to the tag in the map
-            elementsAndScores.put(elementMock, entry.getValue());
-        }
-
-        assert elementsAndScores.size() == scoreMap.size();
-
-        return elementsAndScores;
-    }
 
 
 
---
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/elementScorers/TestHelper.java
@@ -0,0 +1,166 @@
+package com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers;
+
+import com.grayben.riskExtractor.htmlScorer.partScorers.TagAndAttribute;
+import org.jsoup.nodes.Attribute;
+import org.jsoup.nodes.Attributes;
+import org.jsoup.nodes.Element;
+import org.jsoup.parser.Tag;
+import org.mockito.Mockito;
+
+import java.util.*;
+
+/**
+ * Created by beng on 21/12/2015.
+ */
+public final class TestHelper {
+
+    private TestHelper(){}
+
+    public static Element stubElement(Tag tag, Attributes attributes){
+        Element element = Mockito.mock(Element.class);
+        Mockito.when(element.tag())
+                .thenReturn(tag);
+        Mockito.when(element.attributes())
+                .thenReturn(attributes);
+
+        return element;
+    }
+
+    public static Tag stubTag(String tagName){
+        Tag tag = Mockito.mock(Tag.class);
+        Mockito.when(tag.isEmpty()).thenReturn(false);
+        Mockito.when(tag.getName()).thenReturn("font");
+
+        return tag;
+    }
+
+    public static Attribute stubAttribute(String key, String value){
+        Attribute attribute = Mockito.mock(Attribute.class);
+        Mockito.when(attribute.getKey()).thenReturn(key);
+        Mockito.when(attribute.getValue()).thenReturn(value);
+
+        return attribute;
+    }
+
+    public static List<Attribute> stubAttributes(Map<String, String> mapping){
+        List<Attribute> attributes = new ArrayList<>();
+        for (Map.Entry<String, String> entry: mapping.entrySet()) {
+            attributes.add(stubAttribute(entry.getKey(), entry.getValue()));
+        }
+
+        assert attributes.size() == mapping.size();
+        return attributes;
+    }
+
+    public static Attributes convertListToAttributes(List<Attribute> attributeList){
+        Attributes attributes = new Attributes();
+        for (Attribute attribute :
+                attributeList) {
+            attributes.put(attribute);
+        }
+        return attributes;
+    }
+
+    public static Map<String, String> randomMap(int number){
+        Map<String, String> map = new HashMap<>();
+        Random rand = new Random();
+        Iterator<Integer> it = rand.ints(number * 2).iterator();
+        while(it.hasNext()){
+            map.put(
+                    it.next().toString(),
+                    it.next().toString()
+            );
+        }
+        assert map.size() == number;
+
+        return map;
+    }
+
+    public static Element
+    mockElementConformingToTagAndAttribute(TagAndAttribute tagAndAttribute){
+        Element elementMock = Mockito.mock(Element.class);
+
+        //the element returns the scored TagAndAttribute.getTag()
+        Mockito.when(elementMock.tag()).thenReturn(tagAndAttribute.getTag());
+
+        //generate the target attribute from the scored key
+        Attribute targetAttributeMock = stubAttribute(
+                tagAndAttribute.getAttribute().getKey(),
+                tagAndAttribute.getAttribute().getValue()
+        );
+
+        //Stub the Attributes randomly - these ones don't matter
+        Attributes attributeMocks = convertListToAttributes(
+                stubAttributes(randomMap(5))
+        );
+
+        //but then add the target attribute
+        attributeMocks.put(targetAttributeMock);
+
+        //the element returns the attributes
+        // containing TagAndAttribute.getAttribute();
+        Mockito.when(elementMock.attributes()).thenReturn(attributeMocks);
+
+        return elementMock;
+    }
+
+    public static Map<Element, Integer>
+    mockElementsAndScoresConformingToTagAndAttributeScoreMap
+            (Map<TagAndAttribute, Integer> scoreMap){
+        Map<Element, Integer> elementsAndScores = new HashMap<>();
+
+        for (Map.Entry<TagAndAttribute, Integer> entry : scoreMap.entrySet()){
+            Element elementMock = mockElementConformingToTagAndAttribute(
+                    entry.getKey()
+            );
+
+            //expect the element mock to be scored
+            //according to the tagAndAttribute in the map
+            elementsAndScores.put(elementMock, entry.getValue());
+        }
+
+        assert elementsAndScores.size() == scoreMap.size();
+
+        return elementsAndScores;
+    }
+
+    public static Element mockElementConformingToTag(Tag tag){
+        Element elementMock = Mockito.mock(Element.class);
+
+        //the element returns the scored Tag
+        Mockito.when(elementMock.tag()).thenReturn(tag);
+
+        //add some dummy attributes to the element
+        //(they shouldn't matter, since only the tag is important)
+        Attributes attributes = convertListToAttributes(
+                stubAttributes(
+                        randomMap(2)
+                )
+        );
+        Mockito.when(elementMock.attributes()).thenReturn(attributes);
+
+        return elementMock;
+    }
+
+    public static Map<Element, Integer>
+    mockElementsAndScoresConformingToTagScoreMap
+            (Map<Tag, Integer> scoreMap){
+
+        Map<Element, Integer> elementsAndScores = new HashMap<>();
+
+        for (Map.Entry<Tag, Integer> entry:
+                scoreMap.entrySet()) {
+
+            Element elementMock = mockElementConformingToTag(entry.getKey());
+
+            // expect the element mock to be scored
+            // according to the tag in the map
+            elementsAndScores.put(elementMock, entry.getValue());
+        }
+
+        assert elementsAndScores.size() == scoreMap.size();
+
+        return elementsAndScores;
+    }
+
+}
