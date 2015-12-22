568b07f6ef3a4908b408205d0cdef313d49cc489
Ben Gray <gray.ben.m@gmail.com>
2015-12-22 17:47:35 +1100

    Scaffold test

--- src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/tagScorers/TagSegmentationScorerTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/tagScorers/TagSegmentationScorerTest.java
@@ -1,14 +1,19 @@
 package com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers;
 
+import org.jsoup.parser.Tag;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.*;
 
 /**
  * Created by beng on 22/12/2015.
  */
-public class TagSegmentationScorerTest extends MapScorerTest {
+@RunWith(MockitoJUnitRunner.class)
+public class TagSegmentationScorerTest extends MapScorerTest<Tag> {
 
     @Before
     public void setUp() throws Exception {
@@ -17,6 +22,24 @@ public class TagSegmentationScorerTest extends MapScorerTest {
 
     @After
     public void tearDown() throws Exception {
+
+    }
+
+    @Override
+    @Test
+    public void test_ScoreReturnsInteger_WhenArgumentIsNotEmpty() throws Exception {
+
+    }
+
+    @Override
+    @Test
+    public void test_ScoreGivesExpectedResult_WhenSimpleInput() throws Exception {
+
+    }
+
+    @Override
+    @Test
+    public void test_InitThrowsNullPointerException_WhenMapParamIsNull() throws Exception {
 
     }
 }
