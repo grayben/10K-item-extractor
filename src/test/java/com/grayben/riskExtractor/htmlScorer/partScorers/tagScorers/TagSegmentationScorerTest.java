834a9052230c6f856e22aaa410f6c1771a5e4438
Ben Gray <gray.ben.m@gmail.com>
2015-12-22 17:46:46 +1100

    Write TagSegmentationScorer

---
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/tagScorers/TagSegmentationScorerTest.java
@@ -0,0 +1,22 @@
+package com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by beng on 22/12/2015.
+ */
+public class TagSegmentationScorerTest extends MapScorerTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
