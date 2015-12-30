0541cf3b8ae0b4ed39035af2e6e21c6380d4396d
Ben Gray <gray.ben.m@gmail.com>
2015-12-30 14:13:48 +1100

    Add four exception tests

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/NodeVisitorTest.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/NodeVisitorTest.java
@@ -1,12 +1,15 @@
 package com.grayben.riskExtractor.htmlScorer.nodeVisitor;
 
+import org.jsoup.nodes.Comment;
+import org.jsoup.nodes.Node;
 import org.jsoup.select.NodeVisitor;
-import org.junit.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
-
-import static org.junit.Assert.fail;
 
 /**
  * Created by beng on 16/12/2015.
@@ -33,35 +36,51 @@ public abstract class NodeVisitorTest {
 
     }
 
-    @Ignore
     @Test
     public void
     test_HeadThrowsNullPointerException_WhenNodeIsNull
             () throws Exception {
-        fail("Test not implemented");
+        thrown.expect(NullPointerException.class);
+
+        Node nullNode = null;
+
+        nodeVisitorSUT.head(nullNode, 1);
     }
 
-    @Ignore
     @Test
     public void
     test_HeadThrowsIllegalArgumentException_WhenDepthBelowZero
             () throws Exception {
-        fail("Test not implemented");
+        thrown.expect(IllegalArgumentException.class);
+
+        Node node = new Comment("a comment here", "a base URI there");
+
+        int depthBelowZero = -1;
+
+        nodeVisitorSUT.head(node, depthBelowZero);
     }
 
-    @Ignore
     @Test
     public void
     test_TailThrowsNullPointerException_WhenNodeIsNull
             () throws Exception {
-        fail("Test not implemented");
+        thrown.expect(NullPointerException.class);
+
+        Node nullNode = null;
+
+        nodeVisitorSUT.tail(nullNode, 1);
     }
 
-    @Ignore
     @Test
     public void
     test_TailThrowsIllegalArgumentException_WhenDepthBelowZero
             () throws Exception {
-        fail("Test not implemented");
+        thrown.expect(IllegalArgumentException.class);
+
+        Node node = new Comment("a comment here", "a base URI there");
+
+        int depthBelowZero = -1;
+
+        nodeVisitorSUT.tail(node, depthBelowZero);
     }
 }
