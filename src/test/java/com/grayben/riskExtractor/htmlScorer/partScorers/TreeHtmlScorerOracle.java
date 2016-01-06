3f9e18ae302218000469a99c6151fb2b2d6e5854
Ben Gray <gray.ben.m@gmail.com>
2016-01-06 18:57:57 +1100

    Stub interface of oracle

---
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/TreeHtmlScorerOracle.java
@@ -0,0 +1,26 @@
+package com.grayben.riskExtractor.htmlScorer.partScorers;
+
+import com.grayben.riskExtractor.htmlScorer.ScoredText;
+import com.grayben.riskExtractor.htmlScorer.TreeHtmlScorer;
+
+import java.io.File;
+
+/**
+ * Created by beng on 6/01/2016.
+ */
+public class TreeHtmlScorerOracle {
+    TreeHtmlScorer getSut(){
+        //TODO: implement
+        return null;
+    }
+
+    File getInput() {
+        //TODO: implement
+        return null;
+    }
+
+    ScoredText getExpectedOutput(){
+        //TODO: implement
+        return null;
+    }
+}
