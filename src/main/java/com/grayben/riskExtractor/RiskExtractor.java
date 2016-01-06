c8642fb2016692062279a98d0d4688d853f306e1
Ben Gray <gray.ben.m@gmail.com>
2016-01-06 11:22:46 +1100

    Make RiskExtractor conform to new IoC from last commit

--- src/main/java/com/grayben/riskExtractor/RiskExtractor.java
+++ src/main/java/com/grayben/riskExtractor/RiskExtractor.java
@@ -1,12 +1,22 @@
 package com.grayben.riskExtractor;
 
+import com.grayben.riskExtractor.htmlScorer.HtmlScorer;
+import com.grayben.riskExtractor.htmlScorer.ScoredText;
+import com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor;
+import com.grayben.riskExtractor.htmlScorer.TreeHtmlScorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.EmphasisElementScorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.SegmentationElementScorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagAndAttributeScorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagEmphasisScorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagSegmentationScorer;
+import org.jsoup.nodes.Element;
+
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
-
-import com.grayben.riskExtractor.htmlScorer.HtmlScorer;
-import com.grayben.riskExtractor.htmlScorer.ScoredText;
-import com.grayben.riskExtractor.htmlScorer.TreeHtmlScorer;
+import java.util.HashSet;
+import java.util.Set;
 
 public class RiskExtractor {
 	
@@ -43,9 +53,27 @@ public class RiskExtractor {
 		float secondsElapsed = (float)(currentTime - startTime)/1000;
 		System.out.println("Time elapsed: " + secondsElapsed + " seconds");
 	}
+
+	private static ScoringAndFlatteningNodeVisitor setupNodeVisitor(){
+		Set<Scorer<Element>> elementScorers = new HashSet<>();
+		elementScorers.add(
+				new SegmentationElementScorer(
+						new TagSegmentationScorer(TagSegmentationScorer.defaultMap())
+				)
+		);
+		elementScorers.add(
+				new EmphasisElementScorer(
+						new TagEmphasisScorer(TagEmphasisScorer.defaultMap()),
+						new TagAndAttributeScorer(TagAndAttributeScorer.defaultMap())
+				)
+		);
+		ScoringAndFlatteningNodeVisitor nv = new ScoringAndFlatteningNodeVisitor(elementScorers);
+		return nv;
+	}
 	
 	private static void testParse(String url){
-		HtmlScorer scorer = new TreeHtmlScorer();
+		ScoringAndFlatteningNodeVisitor nv = setupNodeVisitor();
+		HtmlScorer scorer = new TreeHtmlScorer(nv);
 		ScoredText scoredText = scorer.scoreHtml(url);
 		System.out.print(scoredText.toString());
 	}
@@ -55,7 +83,7 @@ public class RiskExtractor {
 		String fileName = args[0];
 		File htmlFile = new File(fileName);
 		String charsetName = args[1];
-		HtmlScorer scorer = new TreeHtmlScorer();
+		HtmlScorer scorer = new TreeHtmlScorer(setupNodeVisitor());
 		ScoredText scoredText = scorer.scoreHtml(htmlFile, charsetName);
 		System.out.print(scoredText.toString());
 		File outFile = new File("/Volumes/MBS Data/EDGAR-Form10K/output.txt");
