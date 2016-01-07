ad3c5be0c2b967a26d9efd1e13e5af9cd7b270f9
Ben Gray <gray.ben.m@gmail.com>
2016-01-07 11:07:45 +1100

    Use tree of AnnotatedElement as seed for HtmlTreeScorerOracle

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/AnnotatedElement.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/AnnotatedElement.java
@@ -10,24 +10,24 @@ import java.util.Map;
 /**
  * Created by beng on 5/01/2016.
  */
-class AnnotatedElement extends Element {
+public class AnnotatedElement extends Element {
 
-    Map<String, Integer> scores;
+    private Map<String, Integer> scores;
 
-    Map<String, Integer> getScores() {
+    public Map<String, Integer> getScores() {
         return Collections.unmodifiableMap(scores);
     }
 
-    AnnotatedElement(Element element, Map<String, Integer> scores) {
+    public AnnotatedElement(Element element, Map<String, Integer> scores) {
         this(element.tag(), element.baseUri(), element.attributes(), scores);
     }
 
-    AnnotatedElement(Tag tag, String baseUri, Attributes attributes, Map<String, Integer> scores) {
+    public AnnotatedElement(Tag tag, String baseUri, Attributes attributes, Map<String, Integer> scores) {
         super(tag, baseUri, attributes);
         this.scores = scores;
     }
 
-    AnnotatedElement(Tag tag, String baseUri, Map<String, Integer> scores) {
+    public AnnotatedElement(Tag tag, String baseUri, Map<String, Integer> scores) {
         super(tag, baseUri);
         this.scores = scores;
     }
