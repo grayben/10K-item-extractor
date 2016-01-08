13552573bf09ce9e72e78e6b345965cbcde4f6ca
Ben Gray <gray.ben.m@gmail.com>
2016-01-08 14:10:30 +1100

    Mandate setup and usage of generators

--- src/test/java/com/grayben/testing/SUTInputAndOutputOracle.java
+++ src/test/java/com/grayben/testing/SUTInputAndOutputOracle.java
@@ -3,27 +3,39 @@ package com.grayben.testing;
 /**
  * Created by beng on 8/01/2016.
  */
-public class SUTInputAndOutputOracle<SUT, I, O>
+public abstract class SUTInputAndOutputOracle<SUT, I, O>
         implements
         SystemUnderTestRetrievable<SUT>,
         InputAndExpectedOutputRetrievable<I, O>
 {
 
-    InputAndExpectedOutputRetrievable<I, O> ioGenerator;
-    SystemUnderTestRetrievable<SUT> sutGenerator;
+    private InputAndExpectedOutputRetrievable<I, O> ioGenerator;
+    private SystemUnderTestRetrievable<SUT> sutGenerator;
+
+    abstract protected void instantiateIoGenerator();
+    abstract protected void instantiateSutGenerator();
 
     @Override
     final public I getInput() {
+        if (ioGenerator == null) {
+            instantiateIoGenerator();
+        }
         return ioGenerator.getInput();
     }
 
     @Override
     final public O getExpectedOutput() {
+        if (ioGenerator == null) {
+            instantiateIoGenerator();
+        }
         return ioGenerator.getExpectedOutput();
     }
 
     @Override
     final public SUT getSUT() {
+        if (sutGenerator == null) {
+            instantiateSutGenerator();
+        }
         return sutGenerator.getSUT();
     }
 }
