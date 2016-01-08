f2dca25ed6ebcec4fc00aace31f513dcd1037f00
Ben Gray <gray.ben.m@gmail.com>
2016-01-08 11:59:12 +1100

    Extract Supplier<Set<Scorer<Element>>> into own class

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/NodeVisitorOracle.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/NodeVisitorOracle.java
@@ -5,10 +5,9 @@ import com.grayben.riskExtractor.htmlScorer.ScoredTextElement;
 import com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor;
 import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.TagAndAttribute;
+import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.ElementScorerSetSupplier;
 import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.EmphasisElementScorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.SegmentationElementScorer;
-import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagAndAttributeScorer;
-import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagEmphasisScorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.TagSegmentationScorer;
 import org.jsoup.nodes.Attributes;
 import org.jsoup.nodes.Element;
@@ -20,7 +19,6 @@ import org.junit.runner.RunWith;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import java.util.*;
-import java.util.function.Supplier;
 
 /**
  * For a nominated test configuration: generates the test input, SUT and expected output.
@@ -92,85 +90,6 @@ public class NodeVisitorOracle {
         generateSut();
         generateAnnotatedInput();
         determineExpectedOutput();
-    }
-
-    private static class ElementScorerSetSupplier implements Supplier<Set<Scorer<Element>>> {
-
-        protected enum Content {
-            SEGMENTATION_ELEMENT_SCORER,
-            EMPHASIS_ELEMENT_SCORER
-        }
-
-        private Set<Content> contents = null;
-
-        private Set<Scorer<Element>> elementScorerSet = null;
-
-        ElementScorerSetSupplier(Set<Content> contents){
-            processInputParams(contents);
-        }
-
-        private void processInputParams(Set<Content> contents) {
-            validateInputParams(contents);
-            this.contents = contents;
-        }
-
-        private void validateInputParams(Set<Content> contents) {
-            if (contents == null) {
-                throw new NullPointerException(
-                        "Set<Content> contents was null"
-                );
-            }
-            if(contents.isEmpty()){
-                throw new IllegalArgumentException(
-                        "Set<Content> contents was empty"
-                );
-            }
-            for (Content content : contents){
-                switch (content){
-                    case EMPHASIS_ELEMENT_SCORER:
-                        break;
-                    case SEGMENTATION_ELEMENT_SCORER:
-                        break;
-                    default:
-                        throw new IllegalArgumentException(
-                                "The content " + content + "was not recognised, despite it being defined. Sorry!"
-                        );
-                }
-            }
-        }
-
-        @Override
-        public Set<Scorer<Element>> get() {
-            if (elementScorerSet == null) {
-                generateElementScorers();
-            }
-            return this.elementScorerSet;
-        }
-
-        private void generateElementScorers() {
-            Set<Scorer<Element>> elementScorers = new HashSet<>();
-            for(Content content : contents){
-                switch (content){
-                    case EMPHASIS_ELEMENT_SCORER:
-                        Scorer<Element> emphasisElementScorer = new EmphasisElementScorer(
-                                new TagEmphasisScorer(TagEmphasisScorer.defaultMap()),
-                                new TagAndAttributeScorer(TagAndAttributeScorer.defaultMap())
-                        );
-                        elementScorers.add(emphasisElementScorer);
-
-                        break;
-
-                    case SEGMENTATION_ELEMENT_SCORER:
-                        Scorer<Element> segmentationElementScorer = new SegmentationElementScorer(
-                                new TagSegmentationScorer(TagSegmentationScorer.defaultMap())
-                        );
-                        elementScorers.add(segmentationElementScorer);
-
-                        break;
-                }
-            }
-            this.elementScorerSet = elementScorers;
-        }
     }
 
     private void instantiateGenerators() {
