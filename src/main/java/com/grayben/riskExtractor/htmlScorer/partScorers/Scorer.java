c63ca330e3d281a2eac19c12a5945e411b877503
Ben Gray <gray.ben.m@gmail.com>
2016-01-12 19:57:51 +1100

    Fix Javadoc reference

--- src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/Scorer.java
+++ src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/Scorer.java
@@ -2,7 +2,8 @@ package com.grayben.riskExtractor.htmlScorer.partScorers;
 
 
 /**
- * Element scorer interface. Provide an implementing class to {@link ScoringAndFlatteningVisitor}.
+ * Element scorer interface. Provide an implementing class to
+ * {@link com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor}.
  * <p/>
  * This interface contains a single method that takes an {@link org.jsoup.nodes.Element} and returns a score.
  * @author beng
