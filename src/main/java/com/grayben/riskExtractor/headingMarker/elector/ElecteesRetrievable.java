b4c81c9844fe90557fa82b92e4b8073406b0bf1a
Ben Gray <gray.ben.m@gmail.com>
2016-02-10 11:45:51 +1100

    Document ElecteesRetrievable

--- src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElecteesRetrievable.java
+++ src/main/java/com/grayben/riskExtractor/headingMarker/elector/ElecteesRetrievable.java
@@ -2,6 +2,14 @@ package com.grayben.riskExtractor.headingMarker.elector;
 
 import org.apache.commons.collections4.list.SetUniqueList;
 
+/**
+ * An interface allowing electees to be retrieved from an object.
+ */
 public interface ElecteesRetrievable {
+
+    /**
+     * @return a {@link SetUniqueList} containing the indices into a list corresponding
+     * to elected entries
+     */
 	SetUniqueList<Integer> getElectees();
 }
