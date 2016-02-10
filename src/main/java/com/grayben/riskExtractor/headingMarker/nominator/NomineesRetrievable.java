d27f7f9e5e0448a6921961e077497d151d64a3de
Ben Gray <gray.ben.m@gmail.com>
2016-02-10 11:50:22 +1100

    Document NomineesRetrievable

--- src/main/java/com/grayben/riskExtractor/headingMarker/nominator/NomineesRetrievable.java
+++ src/main/java/com/grayben/riskExtractor/headingMarker/nominator/NomineesRetrievable.java
@@ -2,7 +2,13 @@ package com.grayben.riskExtractor.headingMarker.nominator;
 
 import org.apache.commons.collections4.list.SetUniqueList;
 
+/**
+ * An interface allowing electees to be retrieved from an object.
+ */
 public interface NomineesRetrievable {
 
+    /**
+     * @return the indices into a list corresponding to elected entries
+     */
 	SetUniqueList<Integer> getNomineeIndices();
 }
