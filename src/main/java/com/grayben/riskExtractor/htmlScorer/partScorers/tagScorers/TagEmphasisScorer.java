65b30f6ffb5c5db65a3d50204ca235a4095b63cf
Ben Gray <gray.ben.m@gmail.com>
2016-02-11 14:37:42 +1100

    Document TagEmphasisScorer

--- src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/tagScorers/TagEmphasisScorer.java
+++ src/main/java/com/grayben/riskExtractor/htmlScorer/partScorers/tagScorers/TagEmphasisScorer.java
@@ -1,19 +1,37 @@
 package com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers;
 
+import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import org.jsoup.parser.Tag;
 
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Scores the emphasis of {@link Tag}s passed in to {@link #score(Tag)}.
+ * <p>
+ * Created by Ben Gray, 2015.
+ */
 public class TagEmphasisScorer
 		extends MapScorer<Tag> {
 
+    /**
+     * The string (uniquely) labelling this class.
+     * <p>
+     * Used to identify this class in a collection of {@link Scorer}s.
+     */
     public static final String SCORE_LABEL = "tag-emphasis";
 
+    /**
+     * Construct with the specified map.
+     * @param tagScores the map with which to construct this instance.
+     */
 	public TagEmphasisScorer(Map<Tag, Integer> tagScores) {
 		super(SCORE_LABEL, tagScores);
 	}
 
+    /**
+     * @return the default score map for this class
+     */
 	public static final Map<Tag, Integer> defaultMap() {
 		
 		//Map<Tag, Integer> tagScores = this.getTagScores();
@@ -31,12 +49,20 @@ public class TagEmphasisScorer
 		
 	}
 
+    /**
+     * Use the underlying {@link MapScorer} to score the specified input.
+     * @param input the input to score
+     * @return the score
+     */
 	@Override
 	public int score(Tag input) {
 		validateTagInput(input);
         return super.score(input);
 	}
 
+    /**
+     * @param input the input to validate
+     */
     public static void validateTagInput(Tag input){
         if(input.getName().isEmpty()){
             throw new IllegalArgumentException(
