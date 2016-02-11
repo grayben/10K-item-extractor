7790ac9fdcb488244feac3d1861475a436f412ed
Ben Gray <gray.ben.m@gmail.com>
2016-02-11 16:57:14 +1100

    Comment out outdated TreeHtmlScorerOracle which is based on deleted base class

--- src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/TreeHtmlScorerOracle.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/partScorers/TreeHtmlScorerOracle.java
@@ -1,24 +1,10 @@
 package com.grayben.riskExtractor.htmlScorer.partScorers;
 
-import com.grayben.riskExtractor.htmlScorer.ScoredText;
-import com.grayben.riskExtractor.htmlScorer.nodeVisitor.AnnotatedElement;
-import com.grayben.riskExtractor.htmlScorer.nodeVisitor.AnnotatedElementTreeAssembler;
-import com.grayben.tools.math.parametricEquation.AdaptedParametricEquation;
-import com.grayben.tools.math.parametricEquation.ParametricEquation;
-import com.grayben.tools.testOracle.ParametricTestOracle;
-import org.apache.commons.lang3.tuple.Triple;
-import org.jsoup.nodes.Element;
-
-import java.io.File;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.Set;
-import java.util.function.Function;
-
 /**
  * Created by beng on 6/01/2016.
  */
-public class TreeHtmlScorerOracle
+public class TreeHtmlScorerOracle {}
+/*
         extends ParametricTestOracle<TreeHtmlScorerOracle.Configuration, File, ScoredText> {
 
     private TreeHtmlScorerOracle(ParametricEquation<Configuration, File, ScoredText> parametricEquation) {
@@ -99,3 +85,4 @@ public class TreeHtmlScorerOracle
         }
     }
 }
+*/
