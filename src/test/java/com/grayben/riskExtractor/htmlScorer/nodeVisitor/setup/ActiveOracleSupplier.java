057dde75aa130c63413c57526ee0f6308a054e19
Ben Gray <gray.ben.m@gmail.com>
2016-02-16 17:49:28 +1100

    Simplify

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/ActiveOracleSupplier.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/ActiveOracleSupplier.java
@@ -7,7 +7,6 @@ import org.jsoup.nodes.Node;
 import org.jsoup.select.NodeTraversor;
 import org.jsoup.select.NodeVisitor;
 
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
@@ -18,38 +17,36 @@ public class ActiveOracleSupplier
 
     @Override
     public ActiveOracle<AnnotatedElement, ScoredText> get() {
-        Function<AnnotatedElement, ScoredText> annotatedElementScoredTextFunction = annotatedElement -> {
 
-            ScoredText scoredText = new ScoredText();
+        ScoredText scoredText = new ScoredText();
 
-            NodeVisitor nodeVisitor = new NodeVisitor() {
+        NodeVisitor nodeVisitor = new NodeVisitor() {
 
-                @Override
-                public void head(Node node, int i) {
-                    if (isAnnotatedElement(node)) {
-                        AnnotatedElement annotatedElement = (AnnotatedElement) node;
-                        scoredText.add(
-                                new ScoredTextElement(annotatedElement.ownText(), annotatedElement.getScores())
-                        );
-                    }
+            @Override
+            public void head(Node node, int i) {
+                if (isAnnotatedElement(node)) {
+                    AnnotatedElement annotatedElement = (AnnotatedElement) node;
+                    scoredText.add(
+                            new ScoredTextElement(annotatedElement.ownText(), annotatedElement.getScores())
+                    );
                 }
+            }
 
-                @Override
-                public void tail(Node node, int i) {
-                    isAnnotatedElement(node);
-                }
+            @Override
+            public void tail(Node node, int i) {
+                isAnnotatedElement(node);
+            }
 
-                private boolean isAnnotatedElement(Node node) {
-                    return node.getClass().equals(AnnotatedElement.class);
-                }
-            };
-
-            NodeTraversor nt = new NodeTraversor(nodeVisitor);
-            nt.traverse(annotatedElement);
-            return scoredText;
+            private boolean isAnnotatedElement(Node node) {
+                return node.getClass().equals(AnnotatedElement.class);
+            }
         };
 
-        return annotatedElementScoredTextFunction::apply;
+        NodeTraversor nt = new NodeTraversor(nodeVisitor);
+        return element -> {
+            nt.traverse(element);
+            return scoredText;
+        };
     }
 
 
