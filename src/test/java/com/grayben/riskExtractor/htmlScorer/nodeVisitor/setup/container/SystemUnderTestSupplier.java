fb8a1f5589669d4fcb27891f9aba17cbddcd38a6
Ben Gray <gray.ben.m@gmail.com>
2016-02-17 16:16:33 +1100

    Inject supplier of element scorer set, not set itself, into SUT Supplier

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/container/SystemUnderTestSupplier.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/container/SystemUnderTestSupplier.java
@@ -2,12 +2,11 @@ package com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.container;
 
 import com.grayben.riskExtractor.htmlScorer.ScoredText;
 import com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor;
-import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
+import com.grayben.riskExtractor.htmlScorer.partScorers.elementScorers.ElementScorerSetSupplier;
 import com.grayben.tools.testOracle.SystemUnderTest;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.NodeTraversor;
 
-import java.util.Set;
 import java.util.function.Supplier;
 
 /**
@@ -16,17 +15,18 @@ import java.util.function.Supplier;
 public class SystemUnderTestSupplier
         implements Supplier<SystemUnderTest<Element, ScoredText>> {
 
-    private final Set<Scorer<Element>> elementScorers;
+    private final ElementScorerSetSupplier elementScorerSetSupplier;
 
-    public SystemUnderTestSupplier(final Set<Scorer<Element>> elementScorers) {
-        this.elementScorers = elementScorers;
+    public SystemUnderTestSupplier(ElementScorerSetSupplier elementScorerSetSupplier) {
+        this.elementScorerSetSupplier = elementScorerSetSupplier;
     }
 
     @Override
     public SystemUnderTest<Element, ScoredText> get() {
 
         return element -> {
-            final ScoringAndFlatteningNodeVisitor nodeVisitor = new ScoringAndFlatteningNodeVisitor(elementScorers);
+            final ScoringAndFlatteningNodeVisitor nodeVisitor
+                    = new ScoringAndFlatteningNodeVisitor(elementScorerSetSupplier.get());
             final NodeTraversor nodeTraversor = new NodeTraversor(nodeVisitor);
             nodeTraversor.traverse(element);
             return nodeVisitor.getFlatText();
