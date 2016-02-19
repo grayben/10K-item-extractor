b2e7e1a5289b510572fdd0ae4092ea306d2ed5d2
Ben Gray <gray.ben.m@gmail.com>
2016-02-19 21:31:56 +1100

    Make SystemUnderTestFunction inner of TestContainerFunction

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/container/TestContainerFunction.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/container/TestContainerFunction.java
@@ -1,11 +1,14 @@
 package com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.container;
 
 import com.grayben.riskExtractor.htmlScorer.ScoredText;
+import com.grayben.riskExtractor.htmlScorer.ScoringAndFlatteningNodeVisitor;
 import com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.annotation.AnnotatedElement;
 import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
+import com.grayben.tools.testOracle.SystemUnderTest;
 import com.grayben.tools.testOracle.testContainer.TestContainer;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.NodeTraversor;
 
 import java.util.Set;
 import java.util.function.Supplier;
@@ -36,5 +39,24 @@ public class TestContainerFunction
                 .oracle(activeOracleSupplier.get())
                 .build();
 
+    }
+
+    /**
+     * Created by Ben Gray on 16/02/2016.
+     */
+    public static class SystemUnderTestFunction
+            implements Supplier<SystemUnderTest<Pair<Set<Scorer<Element>>, ? extends Element>, ScoredText>> {
+
+        @Override
+        public SystemUnderTest<Pair<Set<Scorer<Element>>, ? extends Element>, ScoredText> get() {
+
+            return pair -> {
+                final ScoringAndFlatteningNodeVisitor nodeVisitor
+                        = new ScoringAndFlatteningNodeVisitor(pair.getLeft());
+                final NodeTraversor nodeTraversor = new NodeTraversor(nodeVisitor);
+                nodeTraversor.traverse(pair.getRight());
+                return nodeVisitor.getFlatText();
+            };
+        }
     }
 }
