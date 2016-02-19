6fa86c60f29dc40cf925b29f0101fc9c92dafe79
Ben Gray <gray.ben.m@gmail.com>
2016-02-19 18:45:59 +1100

    Revert to Supplier, pass scorers on init

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/annotation/AnnotatedElementSupplier.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/annotation/AnnotatedElementSupplier.java
@@ -5,16 +5,21 @@ import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import org.jsoup.nodes.Element;
 
 import java.util.Set;
-import java.util.function.Function;
 import java.util.function.Supplier;
 
 /**
  * Created by Ben Gray on 17/02/2016.
  */
-public class AnnotatedElementFunction implements Function<Supplier<Set<Scorer<Element>>>, AnnotatedElement> {
+public class AnnotatedElementSupplier implements Supplier<AnnotatedElement> {
+
+    private final Supplier<Set<Scorer<Element>>> scorersSupplier;
+
+    public AnnotatedElementSupplier(Supplier<Set<Scorer<Element>>> scorersSupplier) {
+        this.scorersSupplier = scorersSupplier;
+    }
 
     @Override
-    public AnnotatedElement apply(Supplier<Set<Scorer<Element>>> scorersSupplier) {
+    public AnnotatedElement get() {
 
         return new AnnotatedElement.TreeAssembler(
                 new ElementListSupplier(scorersSupplier.get()).get(),
