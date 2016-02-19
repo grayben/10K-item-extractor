97495afad102eef811580408924021f5ab4a2060
Ben Gray <gray.ben.m@gmail.com>
2016-02-19 18:20:58 +1100

    Inject scorers dependency into AnnotatedElementSupplier at construction

--- src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/annotation/AnnotatedElementSupplier.java
+++ src/test/java/com/grayben/riskExtractor/htmlScorer/nodeVisitor/setup/annotation/AnnotatedElementSupplier.java
@@ -1,7 +1,6 @@
 package com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.annotation;
 
 import com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.ElementListSupplier;
-import com.grayben.riskExtractor.htmlScorer.nodeVisitor.setup.ElementScorersSupplier;
 import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import org.jsoup.nodes.Element;
 
@@ -12,14 +11,19 @@ import java.util.function.Supplier;
  * Created by Ben Gray on 17/02/2016.
  */
 public class AnnotatedElementSupplier implements Supplier<AnnotatedElement> {
+
+    private final Set<Scorer<Element>> scorers;
+
+    public AnnotatedElementSupplier(Set<Scorer<Element>> scorers) {
+        this.scorers = scorers;
+    }
+
     @Override
     public AnnotatedElement get() {
 
-        Set<Scorer<Element>> elementScorers = new ElementScorersSupplier().get();
-
         return new AnnotatedElement.TreeAssembler(
-                new ElementListSupplier(elementScorers).get(),
-                elementScorers
+                new ElementListSupplier(scorers).get(),
+                scorers
         ).getRootAnnotation();
     }
 }
