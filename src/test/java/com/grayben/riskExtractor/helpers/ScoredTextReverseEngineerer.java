3bb7253ea7c3f85cba0113622fa3970935fbc0c5
Ben Gray <gray.ben.m@gmail.com>
2016-04-20 16:25:59 +1000

    Add static methods which reverse engineer ScoredText

---
+++ src/test/java/com/grayben/riskExtractor/helpers/ScoredTextReverseEngineerer.java
@@ -0,0 +1,46 @@
+package com.grayben.riskExtractor.helpers;
+
+import com.grayben.riskExtractor.htmlScorer.ScoredText;
+import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
+import org.apache.commons.io.IOUtils;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
+
+import java.io.InputStream;
+import java.util.Set;
+
+/**
+ * Created by beng on 20/04/2016.
+ */
+public class ScoredTextReverseEngineerer {
+
+    public static Node treeFrom(ScoredText scoredText, Set<Scorer<Element>> elementScorers){
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    public static String htmlFrom(Node tree){
+        StringBuilder sb = new StringBuilder();
+        tree.html(sb);
+        return sb.toString();
+    }
+
+    public static String htmlFrom(ScoredText scoredText, Set<Scorer<Element>> elementScorers){
+        Node tree = treeFrom(scoredText, elementScorers);
+        return htmlFrom(tree);
+    }
+
+    public static InputStream inputStreamFrom(String html){
+        return IOUtils.toInputStream(html);
+    }
+
+    public static InputStream inputStreamFrom(Node tree){
+        String html = htmlFrom(tree);
+        return inputStreamFrom(html);
+    }
+
+    public static InputStream inputSteamFrom(ScoredText scoredText, Set<Scorer<Element>> elementScorers){
+        String html = htmlFrom(scoredText, elementScorers);
+        return inputStreamFrom(html);
+    }
+
+}
