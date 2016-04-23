6955818ef522394d7cb209c84ac4aa7988692888
Ben Gray <gray.ben.m@gmail.com>
2016-04-23 17:35:40 +1000

    Implement randomPairedScoredTextAndHtml and correct spelling of randomScoredText

--- src/test/java/com/grayben/riskExtractor/helpers/ScoredTextGenerator.java
+++ src/test/java/com/grayben/riskExtractor/helpers/ScoredTextGenerator.java
@@ -4,6 +4,7 @@ import com.grayben.riskExtractor.htmlScorer.ScoredText;
 import com.grayben.riskExtractor.htmlScorer.ScoredTextElement;
 import com.grayben.riskExtractor.htmlScorer.partScorers.Scorer;
 import com.grayben.riskExtractor.htmlScorer.partScorers.tagScorers.MapScorer;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.jsoup.nodes.Element;
 import org.jsoup.parser.Tag;
@@ -16,8 +17,55 @@ import java.util.*;
 public class ScoredTextGenerator {
 
     public static Pair<ScoredText, String> randomPairedScoredTextAndHtml(Random random, Set<Scorer<Element>> elementScorers) {
-        throw new UnsupportedOperationException("Not implemented");
+        Set<MapScorer<Tag>> tagScorers = ScoredTextReverseEngineerer.equivalentScorersFrom(elementScorers);
+        List<MapScorer<Tag>> scorerList = new ArrayList<>();
+        scorerList.addAll(tagScorers);
+
+        int numScorers = scorerList.size();
+
+        ScoredText scoredText = new ScoredText();
+        StringBuilder htmlBuilder = new StringBuilder();
+        for(int i = 0; i < random.nextInt(); i++){
+            List<Tag> tagsToApply = new ArrayList<>();
+
+            int maxNumScorersToUse = random.nextInt() % (numScorers + 1);
+            List<MapScorer<Tag>> scorersCopy = new ArrayList<>(scorerList);
+
+            for(int j = 0; j < maxNumScorersToUse; j++){
+                if(random.nextInt() % 2 > 0){
+                    int scorersCopySize = scorersCopy.size();
+                    MapScorer<Tag> selectedScorer = scorersCopy.remove(random.nextInt() % scorersCopySize);
+                    List<Tag> tagList = new ArrayList<>();
+                    tagList.addAll(selectedScorer.getScoresMap().keySet());
+                    tagsToApply.add(tagList.get(random.nextInt() % tagList.size()));
+                } else {
+                    tagsToApply.add(Tag.valueOf("foobarbaz"));
+                }
+
+            }
+            Iterator<Tag> tagIterator = tagsToApply.iterator();
+            String text = ((Integer)random.nextInt()).toString();
+            Element element = new Element(tagIterator.next(), "").text(text);
+            while(tagIterator.hasNext()){
+                Element parent = new Element(tagIterator.next(), "");
+                parent.appendChild(element);
+                element = parent;
+            }
+
+            htmlBuilder.append(element.html());
+
+            ScoredTextElement scoredTextElement;
+            Map<String, Integer> elementScores = new HashMap<>();
+            for(Scorer<Element> scorer: elementScorers){
+                elementScores.put(scorer.getScoreLabel(), scorer.score(element));
+            }
+
+            scoredTextElement = new ScoredTextElement(text, elementScores);
+            scoredText.add(scoredTextElement);
+        }
+        return new ImmutablePair<>(scoredText, htmlBuilder.toString());
     }
+
 
     public static ScoredText randomScoredText(Random random, Set<Scorer<Element>> elementScorers){
         Set<MapScorer<Tag>> tagScorers = ScoredTextReverseEngineerer.equivalentScorersFrom(elementScorers);
@@ -45,7 +93,7 @@ public class ScoredTextGenerator {
             }
             String text = ((Integer)random.nextInt()).toString();
 
-            scoredText.add(new ScoredTextElement(text, textScores);
+            scoredText.add(new ScoredTextElement(text, textScores));
         }
         return scoredText;
     }
