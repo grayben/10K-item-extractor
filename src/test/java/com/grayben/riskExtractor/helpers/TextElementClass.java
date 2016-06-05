3f69aa6fdc38e1e7b26e7ff5b9e2d699f0355e7b
Ben Gray <gray.ben.m@gmail.com>
2016-06-05 13:39:55 +1000

    Put default TextElementClassListDefaults members into TextElementClass

--- src/test/java/com/grayben/riskExtractor/helpers/TextElementClass.java
+++ src/test/java/com/grayben/riskExtractor/helpers/TextElementClass.java
@@ -1,5 +1,8 @@
 package com.grayben.riskExtractor.helpers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by beng on 8/12/2015.
  */
@@ -12,6 +15,84 @@ public enum TextElementClass {
 
     TextElementClass(String archetype){
         this.archetype = archetype;
+    }
+
+    public static List<TextElementClass> defaultClassifications(){
+        List<TextElementClass> list = new ArrayList<>();
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NOMINATED_HEADING);
+        list.add(NOMINATED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(ELECTED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NOMINATED_HEADING);
+        list.add(NOMINATED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(ELECTED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NOMINATED_HEADING);
+        list.add(NOMINATED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(ELECTED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+
+        return list;
+    }
+
+    public static List<TextElementClass> emptyList(){
+        return new ArrayList<>();
+    }
+
+    public static List<TextElementClass> noHeadingList() {
+        List<TextElementClass> list = new ArrayList<>();
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+
+        return list;
+    }
+
+    public static List<TextElementClass> noElectedHeadingList(){
+        List<TextElementClass> list = new ArrayList<>();
+        list.add(NON_HEADING_CONTENT);
+        list.add(NOMINATED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+
+        return list;
+    }
+
+    public static List<TextElementClass> solelyTargetTextList(){
+        List<TextElementClass> list = new ArrayList<>();
+        list.add(ELECTED_HEADING);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+
+        return list;
+    }
+
+    public static List<TextElementClass> electedHeadingOnlyAtEndList(){
+        List<TextElementClass> list = new ArrayList<>();
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(NON_HEADING_CONTENT);
+        list.add(ELECTED_HEADING);
+
+        return list;
     }
 
     public String archetype(){
