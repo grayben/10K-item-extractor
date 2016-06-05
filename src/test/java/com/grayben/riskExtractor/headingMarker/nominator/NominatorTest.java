5e09f642f5392d7ffb420d7f2dd648207ff1e5b6
Ben Gray <gray.ben.m@gmail.com>
2016-06-05 13:53:41 +1000

    Scaffold some tests

---
+++ src/test/java/com/grayben/riskExtractor/headingMarker/nominator/NominatorTest.java
@@ -0,0 +1,55 @@
+package com.grayben.riskExtractor.headingMarker.nominator;
+
+import com.grayben.riskExtractor.headingMarker.Nominator;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.junit.Assert.fail;
+
+/**
+ * Created by beng on 5/06/2016.
+ */
+@RunWith(MockitoJUnitRunner.class)
+public class NominatorTest {
+
+    Nominator nominatorSUT;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Before
+    public void setUp() throws Exception {
+        nominatorSUT = new Nominator(x -> true);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    @Ignore
+    public void test_InitThrowsNullPointerException_WhenPredicateIsNull
+            () throws Exception {
+        thrown.expect(NullPointerException.class);
+        nominatorSUT = new Nominator(null);
+    }
+
+    @Test
+    @Ignore
+    public void test_NominateThrowsNullPointerException_WhenScoredTextIsNull
+            () throws Exception {
+        thrown.expect(NullPointerException.class);
+        nominatorSUT.nominate(null);
+    }
+
+    @Test
+    @Ignore
+    public void test_NominateReturnsExpectedResult
+            () throws Exception {
+        fail("Not implemented");
+    }
+
+}
